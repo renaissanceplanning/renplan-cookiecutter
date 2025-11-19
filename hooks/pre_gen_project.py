ef6858dfc182491fecd1182f4a67e4678c00fd81
Charles Rudder <crudder@citiesthatwork.com>
2025-11-19 10:12:44 -0500

    error handling in pre_gren_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,12 +1,20 @@
 import re
 import sys
+import traceback
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{cookiecutter.project_name}}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print(f'ERROR: The project slug (module_name) is not a valid Python module name. Please do not use a - and use _ '
-          f'instead')
+try:
+    MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+    module_name = '{{cookiecutter.project_name}}'
 
-    # Exit to cancel project
+    print(f"DEBUG: module_name = '{module_name}'")
+
+    if not re.match(MODULE_REGEX, module_name):
+        print(f'ERROR: "{module_name}" is not a valid Python module name.')
+        sys.exit(1)
+except Exception as e:
+    print(f"EXCEPTION in hook: {e}")
+    traceback.print_exc()
     sys.exit(1)
